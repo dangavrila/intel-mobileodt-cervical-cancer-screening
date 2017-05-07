26ef81bd38613106e09bd28035575869b8348b56
dan gavrila <dan.gavrila3@gmail.com>
2017-05-07 19:04:17 +0300

    setup project

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from skimage.io import imread, imshow
+import cv2
+
+%matplotlib inline
+import plotly.offline as py
+py.init_notebook_mode(connected=True)
+import plotly.graph_objs as go
+import plotly.tools as tls
